c0dbff8970d2f80ac66f01f9829443cf33e92e87
AleBles <a.bles@orangegames.com>
2016-06-14 14:20:55 +0200

    updated libs a tad

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 module.exports = function (grunt) {
     'use strict';
 
@@ -9,7 +10,7 @@ module.exports = function (grunt) {
             server: {
                 options: {
                     port: 8080,
-                    base: ['_build/dev', 'node_modules']
+                    base: ['_build/dev', 'node_modules', '../../tools/phaser/dist']
                 }
             }
         },
@@ -30,8 +31,8 @@ module.exports = function (grunt) {
                     'node_modules/phaser-cachebuster/build/phaser-cachebuster.d.ts',
                     'node_modules/phaser-input/build/phaser-input.d.ts',
                     'node_modules/quartz-storage/bin/quartz-storage.d.ts',
-                    'node_modules/multi-pass/build/multi-pass.d.ts',
-                    'node_modules/funny-games-splash/build/funny-games-splash.d.ts'
+                    'node_modules/orange-games-splash/build/orange-games-splash.d.ts',
+                    'node_modules/orange-games-branding/build/orange-games-branding.d.ts'
                 ],
                 noImplicitAny:true
             },
@@ -102,12 +103,19 @@ module.exports = function (grunt) {
                         'node_modules/phaser-input/build/phaser-input.min.js',
                         'node_modules/webfontloader/webfontloader.js',
                         'node_modules/quartz-storage/bin/quartz-storage.js',
-                        'node_modules/funny-games-splash/build/funny-games-splash.min.js',
-                        'node_modules/multi-pass/build/multi-pass.min.js',
+                        'node_modules/orange-games-splash/build/orange-games-splash.min.js',
+                        'node_modules/orange-games-branding/build/orange-games-branding.min.js',
                         '_build/dist/<%= game.name %>-<%= game.version %>.js'
 
                     ]
                 }
+            }
+        },
+        aws_s3: {
+            production: {
+                files: [
+                    {expand: true, cwd: '_build/dist', src: ['**'], dest: '<%= game.name %>/'}
+                ]
             }
         },
         clean: {
@@ -149,6 +157,157 @@ module.exports = function (grunt) {
         grunt.file.write('_build/dist/version.js', 'version="' + buildNumber + '";'  );
     });
 
+    /**
+     * This can decrypt a databag with a given password.
+     * We do this for deploy/cname registration to get the user/pass for aws/cloudflare in the simplest possible secure way
+     *
+     * @param databag
+     * @param password
+     * @returns {*}
+     */
+    function decryptDataBag(databag, password)
+    {
+        var decipher = crypto.createDecipher('aes-256-cbc', password);
+
+        var decrypted = decipher.update(databag, 'hex');
+        decrypted += decipher.final('utf8');
+
+        return decrypted;
+    }
+
+    /**
+     * Used for pushing the code to AWS
+     */
+    grunt.registerTask('deploy', "Upload the game to Amazon s3 bucket", function () {
+        //Setup the required variables, get them from the commandline parameters
+        var databag = grunt.option('databag'),
+            password = grunt.option('password'),
+            bucket = grunt.option('bucket'),
+            region = grunt.option('region');
+
+        if (undefined === databag) {
+            grunt.fail.warn('Can not deploy without an databag containing a user and password');
+        }
+
+        if (undefined === password) {
+            grunt.fail.warn('Can not deploy without an password to decrypt the databag');
+        }
+
+        if (undefined === bucket || undefined === region) {
+            grunt.fail.warn('You didnt specify a bucket/region, so I have no idea where to upload to..');
+        }
+
+        //Decrypt the databag
+        var decryptedData = null;
+        try {
+            decryptedData = JSON.parse(decryptDataBag(databag, password));
+        } catch(e) {
+            grunt.fail.warn('Unable to decrypt databag!');
+        }
+
+        if (!decryptedData.hasOwnProperty('aws')) {
+            grunt.fail.warn('Databag was decrypted but has incorrect data!');
+        }
+
+        //Set the config
+        grunt.config.set('aws_s3.options.accessKeyId', decryptedData.aws.user);
+        grunt.config.set('aws_s3.options.secretAccessKey', decryptedData.aws.pass);
+        grunt.config.set('aws_s3.options.region', region);
+        grunt.config.set('aws_s3.production.options.bucket', bucket);
+
+        //Upload to S3
+        grunt.task.run('aws_s3:production')
+    });
+
+    /**
+     * This Checks if a subdomain is registered at cloudflare or not, and adds it if it isn't registered
+     */
+    grunt.registerTask('setCname', "Set a CNAME at CloudFlare", function () {
+        //Setup the required variables, get them from the commandline parameters
+        var databag = grunt.option('databag'),
+            password = grunt.option('password'),
+            zone = grunt.option('zone'),
+            location = grunt.option('location');
+
+        if (undefined === databag) {
+            grunt.fail.warn('Can not deploy without an databag containing a user and password');
+        }
+
+        if (undefined === password) {
+            grunt.fail.warn('Can not deploy without an password to decrypt the databag');
+        }
+
+        if (undefined === zone) {
+            grunt.fail.warn('You didnt specify a CloudFlare zone, so I have no idea where to set the CNAME record..');
+        }
+
+        if (undefined === location) {
+            grunt.fail.warn('You didnt specify a location to point the CNAME record to.');
+        }
+
+        //Decrypt the databag
+        var decryptedData = null;
+        try {
+            decryptedData = JSON.parse(decryptDataBag(databag, password));
+        } catch(e) {
+            grunt.fail.warn('Unable to decrypt databag!');
+        }
+
+        if (!decryptedData.hasOwnProperty('aws')) {
+            grunt.fail.warn('Databag was decrypted but has incorrect data!');
+        }
+
+        //Set the config
+        grunt.initConfig({
+            game: grunt.file.readJSON('package.json'),
+            http: {
+                getCname: {
+                    options: {
+                        url: 'https://api.cloudflare.com/client/v4/zones/' + zone + '/dns_records?name=<%= game.name %>.fbrq.io',
+                        method: 'GET',
+                        headers: {
+                            'X-Auth-Email': decryptedData.cloudflare.user,
+                            'X-Auth-Key': decryptedData.cloudflare.pass,
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                },
+                setCname: {
+                    options: {
+                        url: 'https://api.cloudflare.com/client/v4/zones/' + zone + '/dns_records',
+                        method: 'POST',
+                        headers: {
+                            'X-Auth-Email': decryptedData.cloudflare.user,
+                            'X-Auth-Key': decryptedData.cloudflare.pass,
+                            'Content-Type': 'application/json'
+                        },
+                        body: '{"type":"CNAME","name":"<%= game.name %>.fbrq.io","content":"' + location + '","ttl":120,"proxied":true‌}'
+                    }
+                }
+            }
+        });
+
+        grunt.config.set('http.getCname.options.callback', function (error, resp, body) {
+            var data = JSON.parse(body);
+
+            if (data.result.length === 0) {
+                grunt.log.ok('No CNAME record for this game yet, adding..');
+                grunt.task.run('http:setCname');
+            } else {
+                grunt.log.ok('CNAME record for game found, continueing without doing anything...');
+            }
+        });
+
+        grunt.config.set('http.setCname.options.callback', function (error, resp, body) {
+            grunt.log.ok('CNAME record for game added!');
+        });
+
+
+        //First we get check if the CNAME already exists
+        grunt.task.run('http:getCname');
+
+    });
+
     //production build, we deploy this
     grunt.registerTask('dist', [
         'tslint:dist',
@@ -162,7 +321,6 @@ module.exports = function (grunt) {
 
     //Development build, used for testing. Starts filewatcher and webserver
     grunt.registerTask('dev', [
-        'tslint:dist',
         'copy:dev',
         'typescript:dev',
         'connect:server',
@@ -175,6 +333,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-aws-s3');
+    grunt.loadNpmTasks('grunt-http');
     grunt.loadNpmTasks('grunt-html-build');
     grunt.loadNpmTasks('grunt-tslint');
 };
