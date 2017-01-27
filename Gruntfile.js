43b425b2ec50312a160554349f33489ba1d9ac9a
AleBles <a.bles@orangegames.com>
2017-01-27 14:02:09 +0100

    Assets split

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
                     {expand: true, cwd: 'assets/css', dest: '_build/dist/assets/css', src: ['**/*']},
                     {expand: true, cwd: 'assets/fonts', dest: '_build/dist/assets/fonts', src: ['**/*']},
                     {expand: true, cwd: 'assets/images', dest: '_build/dist/assets/images', src: ['**/*']},
-                    {expand: true, cwd: 'assets/atlas', dest: '_build/dist/assets/atlas', src: ['**/*']},
+                    {expand: true, cwd: 'assets/atlas', dest: '_build/dist/assets/atlas', src: ['**/*', '!**/*.tps']},
                     {expand: true, cwd: 'assets/spine', dest: '_build/dist/assets/spine', src: ['**/*']},
                     {expand: true, cwd: 'assets/sound', dest: '_build/dist/assets/sound', src: ['**/*', '!**/*.wav']}
                 ]
